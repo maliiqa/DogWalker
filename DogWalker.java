0f83ec6fa1ca0175a9106cbb8b6d27838e4a1b7a
maliiqa <maliiqa@nycstudents.net>
2025-09-26 15:03:01 -0400

    Add files via upload

---
+++ DogWalker.java
@@ -0,0 +1,24 @@
+public class DogWalker
+{
+    private int maxDogs;
+    private DogWalkCompany company;
+    public DogWalker(int max, DogWalkCompany comp)
+    {
+        maxDogs = max;
+        company = comp;
+    }
+    public int walkDogs(int hour)
+    {
+        int dogs = company.numAvailableDogs(hour);
+        if (dogs < maxDogs)
+        {
+            company.updateDogs(hour, dogs);
+            return dogs;
+        }
+        else
+        {
+            company.updateDogs(hour, maxDogs);
+            return maxDogs;
+        }
+    }
+}
